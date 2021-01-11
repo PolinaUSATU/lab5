eee8c287e238aa2d0b7eef68f2fe898a5e5f43f5
PolinaUSATU <77275207+PolinaUSATU@users.noreply.github.com>
2021-01-11 16:32:21 +0500

    include engine

---
+++ engine/connection/connection.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user = 'f0504135_user';
+$password = 'root';
+$database = 'f0504135_db';
+?>
---
+++ engine/page/title.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+	<head>
+        <?require_once 'head.php';?>
+        <title>
+            Каменева Полина Лабораторная работа №5
+        </title>
+	</head>
+</html>
